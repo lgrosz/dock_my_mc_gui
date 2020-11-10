f1fee8793a28eba30c4f08ea60b58a707ca2f858
Logan Grosz <Logan.Grosz@gmail.com>
2020-11-09 23:27:13 -0700

    Serving the built react app

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import path, {dirname} from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.get("/", (req, res) => {
-  res.send("Hi, from express");
-});
+// serve the front-end
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
 // start express server on port 5000
 // todo get port from environment
@@ -12,3 +14,4 @@ const PORT = 8080
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
+

ac1f39ebe573db21ab29fabfe9905d0277ce82dd
Logan Grosz <Logan.Grosz@gmail.com>
2020-11-10 12:35:14 -0700

    Added an echo web socket

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 import path, {dirname} from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
+const server = http.createServer(app);
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // serve the front-end
@@ -12,10 +16,19 @@ app.get('/hello', (_, res) => {
   res.send('Hello from express!');
 });
 
-// start express server on port 5000
-// todo get port from environment
-const PORT = 8080
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
+const wss = new WebSocket.Server({ server: server, path: '/echo' });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected.')
+  ws.on('message', msg => {
+    ws.send(`[Echo]: ${msg}`);
+  });
 });
+
+// start express server on port 5000
+// todo get port from environment - actually maybe not since this is meant to be run with docker - maybe put the port in the docker img
+const PORT = 8080
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+})
 

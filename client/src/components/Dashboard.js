3870f4c444ee1d9a1a5cc1e95530d29baf0ff9cd
Logan Grosz <Logan.Grosz@gmail.com>
2020-11-16 09:34:04 -0700

    Connecting to web socket
    Can also start and stop server. Data is logged to the console.

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,9 +3,25 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 export default function Dashboard() {
   const [response, setResponse] = React.useState();
+  const [ws, setWs] = React.useState();
+
+  React.useEffect(() => {
+    const ws = new WebSocket('ws://localhost:8080/console');
+
+    ws.onopen = () => {
+      console.log('Connected to web socket.');
+    };
+
+    ws.onmessage = (msg) => {
+      console.log(msg.data);
+    };
+
+    setWs(ws);
+  }, []);
 
   const makeRequest = async () => {
     const r = await fetch('/hello');
@@ -14,11 +30,22 @@ export default function Dashboard() {
     setResponse(rText);
   }
 
+  const startServer = () => {
+    ws.send('start');
+  }
+
+  const stopServer = () => {
+    ws.send('stop');
+  }
+
   return (
     <Container>
       <Row>
-        <Button onClick={makeRequest}>Hello</Button>
-        </Row>
+        <ButtonGroup>
+          <Button onClick={startServer}>Start</Button>
+          <Button onClick={stopServer}>Stop</Button>
+        </ButtonGroup>
+      </Row>
       <Row>{JSON.stringify(response)}</Row>
     </Container>
   );

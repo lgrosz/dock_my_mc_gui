3d3b8e3e5ab0efe8466051afe8d3c6f5d95813be
Logan Grosz <Logan.Grosz@gmail.com>
2020-11-16 10:27:19 -0700

    Writing logs to dom

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,12 +2,25 @@ import React from 'react';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import styled from 'styled-components'
+
+// todo componentize the parts of the dashboard
+
+const LogContainer = styled.pre`
+  height: 75vh;
+  overflow-y: auto;
+`
+
+const Log = styled.pre`
+  white-space: pre-wrap;
+`
 
 export default function Dashboard() {
-  const [response, setResponse] = React.useState();
   const [ws, setWs] = React.useState();
+  const [logs, setLogs] = React.useState([]);
 
   React.useEffect(() => {
     const ws = new WebSocket('ws://localhost:8080/console');
@@ -17,18 +30,13 @@ export default function Dashboard() {
     };
 
     ws.onmessage = (msg) => {
+      logs.push(msg.data);
+      setLogs([...logs]);
       console.log(msg.data);
     };
 
     setWs(ws);
   }, []);
-
-  const makeRequest = async () => {
-    const r = await fetch('/hello');
-    const rText = await r.text();
-
-    setResponse(rText);
-  }
 
   const startServer = () => {
     ws.send('start');
@@ -46,7 +54,22 @@ export default function Dashboard() {
           <Button onClick={stopServer}>Stop</Button>
         </ButtonGroup>
       </Row>
-      <Row>{JSON.stringify(response)}</Row>
+      <Row>
+        <Form>
+          <Form.Group>
+            <Form.Control as="div">
+              <LogContainer>
+                {logs.map(log => <Log>{log}</Log>)}
+              </LogContainer>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Control type="text" placeholder="say Hi" />
+          </Form.Group>
+        </Form>
+      </Row>
+      <Row>
+      </Row>
     </Container>
   );
 }
